62d802bc1cd106fc85ba785d4afb4aa5499b7d49
moon <895878077@qq.com>
2020-11-18 20:16:17 +0800

    test

---
+++ src/main/java/second/First.java
@@ -0,0 +1,15 @@
+package second;
+
+public class First {
+    public static void main(String[] args) {
+        try {
+
+            long l = System.currentTimeMillis();
+            if (l > -1) {
+
+            }
+        } finally {
+
+        }
+    }
+}
